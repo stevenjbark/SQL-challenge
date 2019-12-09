1a6c76b50b741724cffeedc786b8a3b3adc78711
Steven J Bark <stevenjbark@gmail.com>
2019-12-08 22:00:04 -0600

    Fourth commit. Tables complete, Assignments 1-4 complete.

--- EmployeeSQL/QuickDBD-export.sql
+++ EmployeeSQL/QuickDBD-export.sql
@@ -102,15 +102,38 @@ LIMIT(20);
 --1. List employee number, last name, first name, gender, salary.
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender, salaries.salary
 FROM salaries
-LEFT JOIN employees
+INNER JOIN employees
 ON salaries.emp_no = employees.emp_no;
 --Checked the output and 300024 rows affected. The employees file has 300025 rows including header.
+SELECT COUNT(emp_no) FROM employees;
 
 --2. List employees hired in 1986. Use % wildcard after hire_date of 1986.
 SELECT *
 FROM employees
 WHERE hire_date LIKE '1986%'
 --Checked output and 30150 rows affected.
+
+--3. List the manager of each department: dept. number, dept. name, manager emp_no, last name, first name, start and end employment dates.
+SELECT dept_manager.dept_no, dept_manager.emp_no, dept_manager.from_date, dept_manager.to_date,
+departments.dept_name, employees.last_name, employees.first_name
+FROM dept_manager
+INNER JOIN departments
+ON departments.dept_no = dept_manager.dept_no
+INNER JOIN employees
+ON employees.emp_no = dept_manager.emp_no;
+--Checked and found 24 rows affected. The dept_manager table has 24 rows, so everything appears OK. 
+SELECT COUNT(dept_no) FROM dept_manager;
+
+--4. List department of each employee: employee number, last name, first name, dept name.
+SELECT dept_emp.emp_no, dept_emp.dept_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+INNER JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments
+ON departments.dept_no = dept_emp.dept_no;
+--Check that dept_emp rows are all OK.
+SELECT COUNT(emp_no) FROM dept_emp;
+
 
 
 
